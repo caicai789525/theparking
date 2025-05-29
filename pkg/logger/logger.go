a03e794e441e3f941f208ed8e8b427a6ebaf85d5
caicai789525 <kevin13537412565@163.com>
2025-05-29 22:10:38 +0800

    port update

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,24 +1,34 @@
-// pkg/logger/logger.go
 package logger
 
 import (
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"os"
 )
 
-var Log *zap.Logger // 导出全局日志实例
+var Log *zap.Logger
 
 func InitLogger(env string) {
-	var err error
+	var config zap.Config
 	if env == "production" {
-		Log, err = zap.NewProduction()
+		config = zap.NewProductionConfig()
 	} else {
-		Log, err = zap.NewDevelopment()
+		config = zap.NewDevelopmentConfig()
 	}
+
+	// 设置日志输出路径
+	file, err := os.OpenFile("/var/log/parking.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		panic(err)
 	}
-}
 
-func Sync() {
-	_ = Log.Sync()
+	// 创建一个新的 zap core 并写入文件
+	core := zapcore.NewCore(
+		zapcore.NewJSONEncoder(config.EncoderConfig),
+		zapcore.AddSync(file),
+		config.Level,
+	)
+
+	// 使用自定义 core 创建一个新的 logger
+	Log = zap.New(core)
 }
